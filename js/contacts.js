8bda947d23f3a64747271f58a7f91d45360c2086
AhmedBenJaballah <127104159+AhmedBenJaballah@users.noreply.github.com>
2023-10-01 21:28:47 +0200

    added succes button contact

--- js/contacts.js
+++ js/contacts.js
@@ -40,6 +40,14 @@ function closeSidebar(){
     }, 50);
 }
 
+function closeSidebar2(){
+    let sidebar=document.getElementById("sidebarLeft")
+    sidebar.style.width="0px"
+    setTimeout(() => {
+    dialog.classList.add("displayNone");
+    }, 50);
+}
+
 function doNotClose(event){
     event.stopPropagation();
 }
@@ -55,7 +63,7 @@ async function createPerson(){
     await setItem('contacts', JSON.stringify(contacts));
     closeSidebar();
     renderContactList(nameC.value,color,emailC.value,phoneC.value);
-  
+    successfullyCreated();
 }
 
 function renderContactList(name,color,email,phone){
@@ -145,7 +153,7 @@ function renderInfo(name,email,color,phone){
             <div class="renderOnlyName">${name}</div>
             <div class="editAndDelete">
                 <div class="alignImg" onclick="modification('${name}','${email}','${color}','${phone}')"><img src="grafiken/edit.png">Edit</div>
-                <div class="alignImg"><img src="grafiken/delete.png">Delete</div>
+                <div class="alignImg" onclick="deleteContact('${name}','${email}','${color}','${phone}')" ><img src="grafiken/delete.png">Delete</div>
             </div>
         </div>
     </div>
@@ -162,11 +170,18 @@ function modification(name,email,color,phone){
     const dialog = document.getElementById("dialog");
     const sidebarLeft = document.getElementById("sidebarLeft");
     const sidebarRight = document.getElementById("sidebarRight");
+    const editForm = document.getElementById("editForm");
+
     dialog.classList.remove("displayNone");
     dialog.classList.add("addSidebar");
     sidebarRight.classList.add("displayNone");
     sidebarLeft.classList.remove("displayNone");
     dialog.style.justifyContent="flex-start";
+    setTimeout(() => {
+        sidebarLeft.style.transition = "width 0.1s ease";
+        sidebarLeft.style.width = "45vw";
+    }, 50);
+    editForm.innerHTML= templatEdit(name,color,email,phone);
     const nameEdit = document.getElementById("nameEdit");
     const emailEdit = document.getElementById("emailEdit");
     const phoneEdit = document.getElementById("phoneEdit");
@@ -175,10 +190,62 @@ function modification(name,email,color,phone){
     emailEdit.value=email
     phoneEdit.value=phone
 
-    
-}   
-
-function editPerson(){
-    nameC.value="f"
-
 }
+
+function templatEdit(name,color,email,phone){
+    return /*html*/`
+    <div class="displayRoundName" style="background-color:${color}">${initials(name)}</div>
+    <form id="formContacts" onsubmit="editPerson('${name}','${email}','${color}','${phone}'); return false;">
+        <input type="name" id="nameEdit" placeholder="Name" class="inputContact" required >
+        <input type="email" id="emailEdit" placeholder="Email" class="inputContact" required>
+        <input type="phone" id="phoneEdit" placeholder="Phone" class="inputContact" required>
+        <div class="btns">
+            <button type="button"  class="btn btn-outline-primary" onclick="deleteContact('${name}','${email}','${color}','${phone}')">Delete <i class="bi bi-x"></i></button>
+            <button  type="submit" id="createBtn" class="btn btn-primary">Save <i class="bi bi-check-lg"></i></button>
+        </div>
+    </form>
+    `
+}
+
+async function editPerson(name,email,color,phone){
+    let contact = contacts.find(u =>u.name==name &&  u.email==email && u.phone== phone)
+    if(contact){
+       console.log(contact)
+       contact.name=nameEdit.value;
+       contact.email=emailEdit.value;
+       contact.phone=phoneEdit.value;
+    }
+    await setItem('contacts', JSON.stringify(contacts));
+    const contactList=document.getElementById("contactList");
+    contactList.innerHTML = "";
+    const display= document.getElementById("displayContact")
+    display.innerHTML="";
+    await loadContacts();
+}
+
+async function deleteContact(name,email,color,phone){
+    const index = contacts.findIndex(c => c.name == name &&  c.email==email && c.phone== phone);
+    contacts.splice(index, 1);
+    await setItem('contacts', JSON.stringify(contacts));
+    const contactList=document.getElementById("contactList");
+    contactList.innerHTML = "";
+    const display= document.getElementById("displayContact")
+    display.innerHTML="";
+    await loadContacts();
+}
+
+function successfullyCreated() {
+    const success = document.getElementById("contactCreated");
+    success.style.display = "flex";
+    success.style.transition = "top 0.5s ease, transform 0.5s ease";
+    setTimeout(() => {
+        success.style.top = "50%"; 
+    }, 50);
+    setTimeout(() => {
+        success.style.top = "100%"; 
+    }, 1500);
+    setTimeout(() => {
+        success.style.display = "none";
+    }, 1800); 
+}
+
