dd77c97cc000649a4c5535d8d7b8122e4391c3d1
Cihad Isen <chihad@live.de>
2023-10-29 21:46:16 +0100

    title added

--- js/policy.js
+++ js/policy.js
@@ -1,17 +1,7 @@
-function policy(element) {
-    // Alle Elemente mit der Klasse "privacyLegalSignUp" auswählen
-    var elements = document.querySelectorAll('.privacyLegalSignUp');
+function policy() {
 
-    // Iteriere durch die ausgewählten Elemente
-    elements.forEach(function(el) {
-      // Entferne die "active"-Klasse von allen Elementen
-      el.classList.remove('active');
-    });
+   }
 
-    // Füge die "active"-Klasse zum angeklickten Element hinzu
-    element.classList.add('active');
-  }
-
-function notice(){
+ notice(){
 
 }
