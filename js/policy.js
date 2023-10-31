baa1704b1b1eb3607faa8e430fe40f954baefc24
Cihad Isen <chihad@live.de>
2023-10-31 15:04:14 +0100

    added style to navbar

--- js/policy.js
+++ js/policy.js
@@ -1,7 +1,20 @@
 function policy() {
+  
+    document.addEventListener("DOMContentLoaded", function () {
+        let currentUrl = window.location.pathname;
 
-   }
+        let links = document.querySelectorAll("#sidebar #menu a");
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].getAttribute("href") === currentUrl) {
+                links[i].classList.add("current-page");
+                break; // Sobald der passende Link gefunden wurde, breche die Schleife ab
+            }
+        }
+    });
 
- notice(){
+
+}
+
+function notice(){
 
 }
