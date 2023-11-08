e0e58a88648df6aeccbc04e74ebfc19c9e71a492
AhmedBenJaballah <127104159+AhmedBenJaballah@users.noreply.github.com>
2023-11-08 17:48:42 +0100

    struktur summary

--- js/summary.js
+++ js/summary.js
@@ -1,77 +1,68 @@
 loadAmount();
-
 setSidebarStyles();
-window.addEventListener('resize', setSidebarStyles);
-
-function setSidebarStyles(){
-  if (window.location.href.includes('summary')) {
-      setTimeout(() => {
-        let summarySidebar = document.getElementById('summarySiebar');
-        const windowWidth = window.innerWidth; 
-
-
-    
-        if (windowWidth < 1040) {
-          summarySidebar.style.backgroundColor = 'transparent';
-          summarySidebar.style.color = '#337aec';
-  
-        } else {
-          summarySidebar.style.backgroundColor = '#D2E3FF';
-          summarySidebar.style.borderRadius = '8px';
-          summarySidebar.style.color = '#42526E';
-        }
-      }, 200);
-    }
-}
-
+window.addEventListener("resize", setSidebarStyles);
 setTimeout(() => {
-    getInitials();
+  getInitials();
 }, 1000);
 
-async function loadAmount() {
-    try {
-      
-       
-    
+function setSidebarStyles() {
+  if (window.location.href.includes("summary")) {
+    setTimeout(() => {
+      let summarySidebar = document.getElementById("summarySiebar");
+      const windowWidth = window.innerWidth;
 
-        const amount = JSON.parse(await getItem('amount'));
-        const tasksToDo = document.getElementById("tasksToDo");
-        const tasksInProgress = document.getElementById("tasksInProgress");
-        const tasksAwaitFeedback = document.getElementById("tasksAwaitFeedback");
-        const tasksDone = document.getElementById("tasksDone");
-        const tasksInBoard = document.getElementById("tasksInBoard");
-        
-        tasksToDo.innerHTML = amount[0]['amountToDo'];
-        tasksInProgress.innerHTML= amount[0]['amountInProgress'];
-        tasksAwaitFeedback.innerHTML = amount[0]['amountAawaitFeedback'];
-        tasksDone.innerHTML = amount[0]['amountDone'];
-        tasksInBoard.innerHTML = amount[0]['totalTasks'];
-        userName= (await getItem('userName'));
-
-        const goodMorning = document.getElementById('goodMornig');
-        const currentTime = new Date();
-        const currentHour = currentTime.getHours();
-
-        let greeting;
-
-        if (currentHour >= 5 && currentHour < 12) {
-            greeting = "Guten Morgen";
-        } else if (currentHour >= 12 && currentHour < 18) {
-            greeting = "Guten Tag";
-        } else if (currentHour >= 18 && currentHour < 22) {
-            greeting = "Guten Abend";
-        } else {
-            greeting = "Gute Nacht";
-        }
-
-        goodMorning.innerHTML = `${greeting}, <span style="color: #4589FF; font-weight: bold; font-size: 60px;">${userName}</span>`;
-
-
-    } catch (e) {
-        console.error('Loading error:', e);
-    }
+      if (windowWidth < 1040) {
+        summarySidebar.style.backgroundColor = "transparent";
+        summarySidebar.style.color = "#337aec";
+      } else {
+        summarySidebar.style.backgroundColor = "#D2E3FF";
+        summarySidebar.style.borderRadius = "8px";
+        summarySidebar.style.color = "#42526E";
+      }
+    }, 200);
+  }
 }
 
-function goToBoard(){
-    window.location.href = 'board.html';
+async function loadAmount() {
+  try {
+    const amount = JSON.parse(await getItem("amount"));
+    const tasksToDo = document.getElementById("tasksToDo");
+    const tasksInProgress = document.getElementById("tasksInProgress");
+    const tasksAwaitFeedback = document.getElementById("tasksAwaitFeedback");
+    const tasksDone = document.getElementById("tasksDone");
+    const tasksInBoard = document.getElementById("tasksInBoard");
+
+    tasksToDo.innerHTML = amount[0]["amountToDo"];
+    tasksInProgress.innerHTML = amount[0]["amountInProgress"];
+    tasksAwaitFeedback.innerHTML = amount[0]["amountAawaitFeedback"];
+    tasksDone.innerHTML = amount[0]["amountDone"];
+    tasksInBoard.innerHTML = amount[0]["totalTasks"];
+    userName = await getItem("userName");
+    greeting();
+  } catch (e) {
+    console.error("Loading error:", e);
+  }
+}
+
+function goToBoard() {
+  window.location.href = "board.html";
+}
+
+function greeting() {
+  const goodMorning = document.getElementById("goodMornig");
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  let greeting;
+
+  if (currentHour >= 5 && currentHour < 12) {
+    greeting = "Guten Morgen";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    greeting = "Guten Tag";
+  } else if (currentHour >= 18 && currentHour < 22) {
+    greeting = "Guten Abend";
+  } else {
+    greeting = "Gute Nacht";
+  }
+
+  goodMorning.innerHTML = `${greeting}, <span style="color: #4589FF; font-weight: bold; font-size: 60px;">${userName}</span>`;
 }
